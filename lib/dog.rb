f4e47c7ef6465ec10faacebcbc7fae7fe59c2190
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 21:00:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
+    attribute_hash = DB[:conn].execute(sql, id)[0]
     Dog.new(attribute_hash)
   end
 
