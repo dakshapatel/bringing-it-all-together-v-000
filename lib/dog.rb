5fc148e7044756dce4b3c11103e82fc3e81b86b5
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 21:09:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,10 +58,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    new_dog = self.new(id: row[0], name: [1], breed: [2])
+    # id = row[0]
+    # name = row[1]
+    # breed = row[2]
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
     new_dog
   end
 
